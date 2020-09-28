2cf72dbb6dceca6d80b64bfff20aaf09102bfc5b
Christian De la Cruz Belmonte <christian.delacruz6257@alumnos.udg.mx>
2020-09-28 08:00:06 -0500

    Se agrego el codigo para solucionar el problema

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var base uint64
+	var height uint64
+
+	fmt.Scanln(&base)
+	fmt.Scanln(&height)
+
+	fmt.Println(base * height / 2)
 }
